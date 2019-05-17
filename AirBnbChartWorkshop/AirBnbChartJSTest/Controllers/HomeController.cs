e9dd560b2c8047fc0c9bb177a209c4c9d71c3003
Rahkiin <DusKwakernaat@gmail.com>
2019-05-17 11:40:33 +0200

    Fixed Reference

---
+++ AirBnbChartWorkshop/AirBnbChartJSTest/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AirBnbChartJSTest.Models;
+using AirBnbChartJSTest.Services;
+using Newtonsoft.Json;
+
+namespace AirBnbChartJSTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ChartsService _chartsService;
+
+        public HomeController()
+        {
+            _chartsService = new ChartsService();
+        }
+
+        public IActionResult Barchart()
+        {
+            var dataPoints = _chartsService.GetListingsPerNeighbourhoodDataPoints();
+            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+
+            return View();
+        }
+
+        public IActionResult Piechart()
+        {
+            var dataPoints = _chartsService.GetListingsPerNeighbourhoodDataPoints();
+            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+
+            return View();
+        }
+
+        public IActionResult Linechart()
+        {
+            var dataPoints = _chartsService.GetListingsPerNeighbourhoodDataPoints();
+            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AirBnbChartWorkshop/AirBnbChartJSTest/Services/ChartsService.cs
@@ -0,0 +1,34 @@
+﻿using AirBnbChartJSTest.Models;
+using AirBnbFakeDatabase.Database;
+using AirBnbFakeDatabase.Services;
+using System.Collections.Generic;
+
+namespace AirBnbChartJSTest.Services
+{
+    public class ChartsService
+    {
+        private readonly FakeDatabase _fakeDatabase;
+        private readonly ListingService _listingService;
+
+        public ChartsService()
+        {
+            _fakeDatabase = new FakeDatabase();
+            _listingService = new ListingService();
+        }
+
+        public List<DataPoint> GetListingsPerNeighbourhoodDataPoints()
+        {
+            var listings = _fakeDatabase.Listings;
+            var amountOfListingsPerNeighbourhood = _listingService.GetBarChartData(listings);
+
+            List<DataPoint> dataPoints = new List<DataPoint>();
+            foreach (var listing in amountOfListingsPerNeighbourhood)
+            {
+                DataPoint datapoint = new DataPoint(listing.NeighbourhoodName, listing.AmountOfListings);
+                dataPoints.Add(datapoint);
+            }
+
+            return dataPoints;
+        }
+    }
+}
